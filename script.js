6ffb4b806a363d2bbe2cbd458e1514376f533951
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2025-12-16 10:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const currentURL = location.href;
+
+
+const urlLength = currentURL.length;
+
+// Display the result using alert
+alert("The length of the URL is: " + urlLength);
